bedcba0b1d64678a6e577afd6cec8cee1891294c
Steven Donath <92764426+stevendonath@users.noreply.github.com>
2025-04-01 19:03:58 -0500

    first commit

---
+++ credentials.php
@@ -0,0 +1,6 @@
+<?php
+$servername = 'localhost';     //if apache on diff server from db or using Docker, set IP address
+$dbname = 'employees';              //which db you're going to use
+$username = 'jens2259';
+$password = 'jens2259';
+?>
